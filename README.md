3f880cfbbec70f7bf3b5a4d46d7e74f2f5167535
dchad <qiaozi888@gmail.com>
2021-04-07 20:30:28 -0700

    MOSFET testing.

--- README.md
+++ README.md
@@ -1,21 +1,5 @@
 # ESP32 Camera
 
-A simple video surveillance application using an ESP32 WROOM (DevkitC v1.0) and an OV7670 camera with a web interface 
-and a GTK application for user control and video streaming.
+Various applications using ESP32-CAM and ESP32 WROOM (DevkitC v1.0) development boards. 
 
-TODO: everything.
-
-
-
-
-## 1. COMPONENTS
-
-
-## 2. CIRCUIT DIAGRAM
-
-
-## 3. PHOTOS
-
-
-## 4. TECHNICAL NOTES
 
--- mosfet-test/mosfet-test.ino
+++ mosfet-test/mosfet-test.ino
@@ -9,8 +9,7 @@
 
 #include <Arduino.h>
 
-
-#define USE_SERIAL Serial
+#define RS232 Serial
 
 
 
@@ -22,20 +21,22 @@ void setup() {
   // WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
 
   
-    USE_SERIAL.begin(115200);
+    RS232.begin(115200);
     
-    USE_SERIAL.println();
-    USE_SERIAL.println();
-    USE_SERIAL.println();
+    RS232.println();
+    RS232.println();
+    RS232.println();
+    RS232.println();
+    RS232.println();
     
-    pinMode(2, OUTPUT);      // set the on-board LED pin mode
-    pinMode(25, OUTPUT);
+    pinMode(2, OUTPUT);      // set the on-board blue LED pin mode
+    pinMode(25, OUTPUT);     // MOSFET gate.
     blink = 0;
     
     for(uint8_t t = 4; t > 0; t--) 
     {
-        USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
-        USE_SERIAL.flush();
+        RS232.printf("Flushing the pipes %d...\n", t);
+        RS232.flush();
         delay(1000);
     }
 
@@ -81,8 +82,25 @@ void loop() {
        digitalWrite(2, LOW);
        delay(125);
     }
-    
-    USE_SERIAL.printf("End of test loop...\n");
+    for(ii = 0; ii < 80; ii++)
+    {
+       digitalWrite(25, HIGH);
+       digitalWrite(2, HIGH);
+       delay(50);
+       digitalWrite(25, LOW);
+       digitalWrite(2, LOW);
+       delay(50);
+    }    
+    for(ii = 0; ii < 160; ii++)
+    {
+       digitalWrite(25, HIGH);
+       digitalWrite(2, HIGH);
+       delay(10);
+       digitalWrite(25, LOW);
+       digitalWrite(2, LOW);
+       delay(10);
+    }    
+    RS232.printf("End of test loop...\n");
 
     delay(1000);
 }
